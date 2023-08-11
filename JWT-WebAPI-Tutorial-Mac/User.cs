b450dcb020adefc022eec7e902f82a3816593d1c
Edward Apostol <ed@edapostol.com>
2023-08-11 12:57:24 -0400

    feat: create User Model

---
+++ JWT-WebAPI-Tutorial-Mac/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace JWT_WebAPI_Tutorial_Mac
+{
+	public class User
+	{
+		public string Username { get; set; } = string.Empty;
+		public byte[]? PasswordHash { get; set; } = null;
+        public byte[]? PasswordSalt { get; set; } = null;
+
+
+
+        public User()
+		{
+		}
+	}
+}
+
