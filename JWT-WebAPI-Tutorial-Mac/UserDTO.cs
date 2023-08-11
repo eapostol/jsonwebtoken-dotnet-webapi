857868d511370cae9f5957a63676c02ac5200d89
Edward Apostol <ed@edapostol.com>
2023-08-11 12:57:36 -0400

    feat: create userDTO

---
+++ JWT-WebAPI-Tutorial-Mac/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace JWT_WebAPI_Tutorial_Mac
+{
+	public class UserDTO
+	{
+		public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+
+        public UserDTO()
+		{
+		}
+	}
+}
+
